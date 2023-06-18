e582059d87e2b2f21964a7721ece0e5b9a00dce7
- ShaKuR .#Evan <96147328+jalithon@users.noreply.github.com>
2023-06-19 00:18:14 +0300

    Update test.py

--- test.py
+++ test.py
@@ -89,7 +89,7 @@ logging.basicConfig(
 LOGS = logging.getLogger(__name__)
 
 
-@sython.on(events.NewMessage(outgoing=True, pattern=r"\.تحديث"))
+@sython1.on(events.NewMessage(outgoing=True, pattern=r"\.تحديث"))
 async def _(event):
 
     sandy = await event.edit(

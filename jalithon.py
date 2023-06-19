a054443a1e7ad11d3a9f5f8a559626ae8c6524c7
- ShaKuR .#Evan <96147328+jalithon@users.noreply.github.com>
2023-06-19 17:49:49 +0300

    Update jalithon.py

--- jalithon.py
+++ jalithon.py
@@ -89,7 +89,64 @@ async def _(event):
 • فحـص السورس** : `.فحص**` 
 .
 """)
-
+    
+@jalithon.on(events.NewMessage(outgoing=True, pattern=r"\.ت"))
+async def _(event):
+    start = datetime.datetime.now()
+    await event.edit("جارٍ...")
+    end = datetime.datetime.now()
+    ms = (end - start).microseconds / 1000
+    await event.edit(f'''
+**☆ تـوب سـورس
+☆ الاصدار : 1.2
+☆ البنك : `{ms}`
+☆ التاريخ : `{m9zpi}`
+☆ الايدي : `{event.sender_id}`
+☆ تـوب سـورس : @Repthon**
+''')   
+@jalithon.on(events.NewMessage(outgoing=True, pattern=r"\.اشتراكاتي"))
+async def _(event):
+    if event.fwd_from:
+        return
+    start = datetime.datetime.now()
+    u = 0  # number of users
+    g = 0  # number of basic groups
+    c = 0  # number of super groups
+    bc = 0  # number of channels
+    b = 0  # number of bots
+    await event.edit("يتم التعداد ..")
+    async for d in jalithon.iter_dialogs(limit=None):
+        if d.is_user:
+            if d.entity.bot:
+                b += 1
+            else:
+                u += 1
+        elif d.is_channel:
+            if d.entity.broadcast:
+                bc += 1
+            else:
+                c += 1
+        elif d.is_group:
+            g += 1
+        else:
+            pass
+            # logger.info(d.stringify())
+    end = datetime.datetime.now()
+    ms = (end - start).seconds
+    await event.edit("""تم استخراجها في {} ثواني
+`الاشخاص :\t{}
+المجموعات العادية :\t{}
+المجموعات الخارقة :\t{}
+القنوات :\t{}
+البوتات :\t{}
+`""".format(ms, u, g, c, bc, b))
+    
+@jalithon.on(events.NewMessage(outgoing=True, pattern=r"\.اعادة تشغيل"))
+async def update(event):
+    await event.edit("• جارِ اعادة تشغيل السورس ..\n• انتضر 1-2 دقيقة  .")
+    await sedthon.disconnect()
+    await sedthon.send_message("me", "`اكتملت اعادة تشغيل السورس !`")
+    
 @jalithon.on(events.NewMessage(outgoing=True, pattern=r"\.فحص"))
 async def _(event):
     start = datetime.datetime.now()

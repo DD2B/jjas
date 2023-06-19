750f7573d3b9e96970c902bdb8cb45e93125fbb4
- ShaKuR .#Evan <96147328+jalithon@users.noreply.github.com>
2023-06-19 07:23:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,12 +2,6 @@ from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 import os
 
-print(
-    """Please go-to my.telegram.org
-Login using your Telegram account
-Click on API Development Tools
-Create a new application, by entering the required details"""
-)
 APP_ID = os.environ.get("APP_ID")
 APP_HASH = os.environ.get("APP_HASH")
 session1 = os.environ.get("TERMUX")
@@ -16,9 +10,5 @@ DEVLOO = os.environ.get("DEVLO")
 CHNA = os.environ.get("CHNA")
 jalithon = TelegramClient(StringSession(session1), APP_ID, APP_HASH)
 
-
-
 ispay = ['yes']
 ispay2 = ['yes']
-
-

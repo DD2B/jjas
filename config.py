9718481bece8d1da3a48123295e744fc8cb7cc2b
- ShaKuR .#Evan <96147328+jalithon@users.noreply.github.com>
2023-06-19 07:18:38 +0300

    Update config.py

--- config.py
+++ config.py
@@ -10,10 +10,8 @@ Create a new application, by entering the required details"""
 )
 APP_ID = os.environ.get("APP_ID")
 APP_HASH = os.environ.get("APP_HASH")
-BOT_USERNAME = ("‏hha2hbot")
 session1 = os.environ.get("TERMUX")
 SESSION1 = os.environ.get("TERMUX")
-token = ("6031928256:AAFJ-WfewxHof8ek37KcXGG3wgN-GhkfCEI")
 DEVLOO = os.environ.get("DEVLO")
 CHNA = os.environ.get("CHNA")
 jalithon = TelegramClient(StringSession(session1), APP_ID, APP_HASH)
@@ -22,5 +20,5 @@ bot = TelegramClient("bot", APP_ID, APP_HASH).start(bot_token=token)
 
 ispay = ['yes']
 ispay2 = ['yes']
-bot.start()
+
 

9772eb758df1edcf8bbe3efb3d6bd1c9b0b66e64
- ShaKuR .#Evan <96147328+jalithon@users.noreply.github.com>
2023-06-19 05:55:19 +0300

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ SESSION1 = os.environ.get("TERMUX")
 token = ("6031928256:AAFJ-WfewxHof8ek37KcXGG3wgN-GhkfCEI")
 DEVLOO = os.environ.get("DEVLO")
 CHNA = os.environ.get("CHNA")
-sython1 = TelegramClient(StringSession(session1), APP_ID, APP_HASH)
+jalithon = TelegramClient(StringSession(session1), APP_ID, APP_HASH)
 bot = TelegramClient("bot", APP_ID, APP_HASH).start(bot_token=token)
 
 

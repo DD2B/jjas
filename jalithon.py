b122d94817841f542ccdd21e4b5b0d549d7737ec
- ShaKuR .#Evan <96147328+jalithon@users.noreply.github.com>
2023-06-22 22:59:46 +0300

    Update jalithon.py

--- jalithon.py
+++ jalithon.py
@@ -191,7 +191,7 @@ async def OwnerStart(event):
                 for i in range(100):
                     await asyncio.sleep(2)
 
-                    list = await sython1(GetHistoryRequest(peer=channel_entity, limit=1,
+                    list = await jalithon(GetHistoryRequest(peer=channel_entity, limit=1,
                                                             offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
                     msgs = list.messages[0]
                     if msgs.message.find('لا يوجد قنوات في الوقت الحالي , قم يتجميع النقاط بطريقه مختلفه') != -1:

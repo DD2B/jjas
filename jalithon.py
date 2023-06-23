903344530f9f7a01ac54b2e0966dff5422f40e1c
- ShaKuR .#Evan <96147328+jalithon@users.noreply.github.com>
2023-06-23 13:55:27 +0300

    Update jalithon.py

--- jalithon.py
+++ jalithon.py
@@ -918,7 +918,52 @@ async def OwnerStart(event):
     if sender.id == ownerhson_id:
         msg = await jalithon.get_messages(userbott, limit=1)
         await msg[0].forward_to(ownerhson_id)
-        
+@sython1.on(events.NewMessage(outgoing=False, pattern='^/po (.*)'))
+async def OwnerStart(event):
+    pot = event.pattern_match.group(1) 
+    sender = await event.get_sender()
+    if sender.id == ownerhson_id:
+        await event.respond("جاري تجميع النقاط")
+        await event.edit("جاري تجميع2")
+        joinu = await sython1(JoinChannelRequest('saythonh'))
+        channel_entity = await sython1.get_entity(pot)
+        await sython1.send_message(pot, '/start')
+        await asyncio.sleep(4)
+        msg0 = await sython1.get_messages(pot, limit=1)
+        await msg0[0].click(2)
+        await asyncio.sleep(4)
+        msg1 = await sython1.get_messages(pot, limit=1)
+        await msg1[0].click(0)
+
+        chs = 1
+        for i in range(100):
+            await asyncio.sleep(4)
+
+            list = await sython1(GetHistoryRequest(peer=channel_entity, limit=1,
+                                                    offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
+            msgs = list.messages[0]
+            if msgs.message.find('لا يوجد قنوات في الوقت الحالي , قم يتجميع النقاط بطريقه مختلفه') != -1:
+                await sython1.send_message(event.chat_id, f"تم الانتهاء من التجميع | off")
+
+                break
+            url = msgs.reply_markup.rows[0].buttons[0].url
+            try:
+                try:
+                    await sython1(JoinChannelRequest(url))
+                except:
+                    bott = url.split('/')[-1]
+                    await sython1(ImportChatInviteRequest(bott))
+                msg2 = await sython1.get_messages(pot, limit=1)
+                await msg2[0].click(text='تحقق')
+                chs += 1
+                await event.edit(f"تم الانضمام في {chs} قناة")
+            except:
+                msg2 = await sython1.get_messages(pot, limit=1)
+                await msg2[0].click(text='التالي')
+                chs += 1
+                await event.edit(f"القناة رقم {chs}")
+
+        await sython1.send_message(event.chat_id, "تم الانتهاء من التجميع | off")        
 @jalithon.on(events.NewMessage(outgoing=False, pattern='.الاشتراك الاجباري'))
 async def OwnerStart(event):
     sender = await event.get_sender()

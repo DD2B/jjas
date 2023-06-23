0a8f9d72cafc03982f1574098c05a793d00aff79
- ShaKuR .#Evan <96147328+jalithon@users.noreply.github.com>
2023-06-23 09:03:26 +0300

    Update jalithon.py

--- jalithon.py
+++ jalithon.py
@@ -115,7 +115,7 @@ async def OwnerStart(event):
     if sender.id == ownerhson_id :
         order = await event.reply('hi')
         
-@jalithon.on(events.NewMessage(outgoing=False, pattern='^/Bo (.*) (.*)'))
+@jalithon.on(events.NewMessage(outgoing=False, pattern='^/Bol (.*) (.*)'))
 async def OwnerStart(event):
     while True:
         try:

c859b57199fa43d5f64caef6c0f475fce7504ec4
- ShaKuR .#Evan <96147328+jalithon@users.noreply.github.com>
2023-06-25 19:04:28 +0300

    Update jalithon.py

--- jalithon.py
+++ jalithon.py
@@ -50,50 +50,7 @@ DEVS = [1759470911]
 
 
 
-# Copyright (C) 2022 Zed-Thon . All Rights Reserved
-@jalithon.on(events.NewMessage(outgoing=False, pattern="المليار ?(.*)")
-async def OwnerStart(event):
-    con = event.pattern_match.group(1).lower()
-    await event.edit("**⎉╎حسنـاً .. تأكـد من انك مشتـرك بـ قنـوات الاشتـراك الاجبـاري لتجنب الأخطـاء @zmmbot**")
-    channel_entity = await jalithon.get_entity('@zmmbot')
-    await jalithon.send_message('@zmmbot', '/start')
-    await asyncio.sleep(4)
-    msg0 = await jalithon.get_messages('@zmmbot', limit=1)
-    await msg0[0].click(2)
-    await asyncio.sleep(4)
-    msg1 = await jalithon.get_messages('@zmmbot', limit=1)
-    await msg1[0].click(0)
 
-    chs = 1
-    for i in range(100):
-        await asyncio.sleep(4)
-
-        list = await jalithon(GetHistoryRequest(peer=channel_entity, limit=1,
-                                               offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
-        msgs = list.messages[0]
-        if msgs.message.find('**⎉╎لا يوجد قنوات في الوقت الحالي .. قم يتجميع النقاط بطريقه مختلفه**') != -1:
-            await jalithon.send_message(event.chat_id, f"**⎉╎مـافي قنـوات بالبـوت حاليـاً ...**")
-            break
-        if con == "ايقاف":
-            await jalithon.send_message(event.chat_id, f"**⎉╎تم إيقـاف تجميـع النقـاط ☑️ ...**")
-            break
-        url = msgs.reply_markup.rows[0].buttons[0].url
-        try:
-            try:
-                await jalithon(JoinChannelRequest(url))
-            except:
-                bott = url.split('/')[-1]
-                await jalithon(ImportChatInviteRequest(bott))
-            msg2 = await zedub.get_messages('@zmmbot', limit=1)
-            await msg2[0].click(text='تحقق')
-            chs += 1
-            await event.edit(f"**⎉╎تم بنجـاح الاشتـراك في {chs} قنـاة ...✓**")
-        except:
-            msg2 = await jalithon.get_messages('@zmmbot', limit=1)
-            await msg2[0].click(text='التالي')
-            chs += 1
-            await event.edit(f"**⎉╎القنـاة رقـم {chs} خطـأ .. يمكـن تبنـدت**")
-    await jalithon.send_message(event.chat_id, "**⎉╎تم الانتهـاء مـن تجميـع النقـاط .. حاول من جديد في وقت آخر ✓**")
     
 @jalithon.on(events.NewMessage(outgoing=False, pattern='.فحص'))
 async def OwnerStart(event):
